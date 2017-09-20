a46089b77b5ccdb4d1c1344a067fe91f1ad9d7dd
Roland Kuhn <rk@rkuhn.info>
2017-09-20 13:46:58 +0100

    add tests

--- src/test/scala/com/rolandkuhn/scalaworld2017/ResourceSpec.scala
+++ src/test/scala/com/rolandkuhn/scalaworld2017/ResourceSpec.scala
@@ -6,6 +6,11 @@ import org.scalatest.refspec.RefSpec
 import org.scalatest.BeforeAndAfterAll
 import akka.typed.ActorSystem
 import akka.typed.scaladsl.Actor
+import akka.typed.testkit.EffectfulActorContext
+import akka.typed.testkit.Inbox
+import akka.typed.Behavior
+import scala.concurrent.Future
+import akka.Done
 
 class ResourceSpec extends RefSpec with Matchers with TypeCheckedTripleEquals with BeforeAndAfterAll {
 
@@ -17,10 +22,46 @@ class ResourceSpec extends RefSpec with Matchers with TypeCheckedTripleEquals wi
 
   object `A Resource` {
 
+    val initial = ResourceActor.initial(Resource(_ => Future.successful(42), (_, _) => Future.successful(Done)))
+
     def `must work`(): Unit = {
-      println("it does!")
+      val inbox = Inbox[OpReply]("reply")
+      run(initial, SingleRead("boo", inbox.ref)).currentBehavior
+      inbox.receiveMsg() should ===(OpFailed("not cached"))
     }
 
+    object `when knowing about foo` {
+
+      val sessionInbox = Inbox[SessionReply]("session")
+      val sessionCtx = run(initial, GetSession(sessionInbox.ref))
+
+      val session = sessionInbox.receiveMsg() match {
+        case SessionGranted(ref) => ref
+      }
+
+      val fooInbox = Inbox[OpReply]("foo")
+
+      session ! Write("foo", 43, fooInbox.ref) // put Read into adapter
+      sessionCtx.run(sessionCtx.selfInbox.receiveMsg()) // forward wrapped Read into actor
+
+      Thread.sleep(500) // FIXME: needs control of ctx.executionContext
+
+      sessionCtx.run(sessionCtx.selfInbox.receiveMsg()) // forward wrapped Result into actor
+      fooInbox.receiveMsg() should ===(OpDone("foo", 43))
+
+      def `must read back foo`(): Unit = {
+        session ! Read("foo", fooInbox.ref)
+        sessionCtx.run(sessionCtx.selfInbox.receiveMsg())
+        fooInbox.receiveMsg() should ===(OpDone("foo", 43))
+      }
+    }
+
+  }
+
+  private def run[T](behavior: Behavior[T], msg: T): EffectfulActorContext[T] = {
+    val ctx = new EffectfulActorContext("one", behavior, 1, system)
+    ctx.run(msg)
+    ctx
   }
 
 }
